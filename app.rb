c35a173c3c8ca630f473466d9a89da154d794ffb
ashleymichal <ashleymichal@gmail.com>
2016-03-13 13:01:17 -0500

    skeleton MVC

--- app.rb
+++ app.rb
@@ -1,5 +1,20 @@
 require 'sinatra'
+require 'sinatra/activerecord'
+require './config/environments' #database configuration
+require './models/recipe.rb'
 
 get '/' do
-  "Hello, World!"
+  erb :index
+end
+
+get '/results' do
+  erb :results
+end
+
+get "/recipe" do
+  erb :card
+end
+
+get "/profile" do
+  erb :profile
 end
